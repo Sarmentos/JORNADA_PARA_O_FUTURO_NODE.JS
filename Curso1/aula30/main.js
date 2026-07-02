46e03225bdff9fac8e97ecf35245be592940e5e4
Sarmentos <lucassilvasarmentos3@gmail.com>
2026-07-02 16:00:11 -0300

    fix: classe

---
+++ Curso1/aula30/main.js
@@ -0,0 +1,22 @@
+const pessoa = {
+    nome:'Lucas',
+    idade: 22,
+    temCnh: true
+};
+
+pessoa.sobrenome = 'Silva';
+
+console.log(pessoa.nome);
+console.log(pessoa.sobrenome);
+
+const livro = {
+    titulo: 'O Hobbit',
+    autor: 'J.R.R. Tolkien',
+    paginas: 300
+};
+
+livro.publicado = true;
+livro.idioma = ['Português', 'Inglês', 'Espanhol'];
+
+console.log(livro.titulo);
+console.log(livro.idioma);

964d8bb15c997ef69682ab3647caae573fc5d530
Sarmentos <lucassilvasarmentos3@gmail.com>
2026-07-06 18:30:52 -0300

    fix: Criando e salvando arquivos

--- Curso3/src/cli/cli.js
+++ Curso3/src/cli/cli.js
@@ -4,12 +4,25 @@ import { contaPalavras } from '../index.js';
 
 const texto = process.argv;
 const link = texto[2];
+const endereco = texto[3];
 
 fs.readFile(link, 'utf-8', (err, data) => {
     try {
         if (err) throw err;
-        contaPalavras(data);
+       const resultado = contaPalavras(data);
+       criaESalvaArquivo(resultado, endereco);
     } catch (err) {
         trataErros(err);
     }
 });
+
+async function criaESalvaArquivo(listaPalavra,endereco) {
+    const arquivo = `${endereco}/resultado.txt`;
+    const textoPalavras = JSON.stringify(listaPalavra, null, 2);
+    try{
+        await fs.promises.writeFile(arquivo, textoPalavras);
+        console.log(`Arquivo salvo em: ${arquivo}`);
+    }catch(err){
+        throw Error;
+    }
+}
